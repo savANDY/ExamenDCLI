f369c6bfcde07867539d96c23e384e574d9fb4a8
savANDY <andicuu@gmail.com>
2017-11-09 13:05:30 +0100

    Ultimos retoques

--- js/leerNoticias.js
+++ js/leerNoticias.js
@@ -4,27 +4,29 @@ var misRoles=["usu","admin","usu"];
 
 function mostrarLogin(x) {
   if (x == "si") {
-  document.getElementById("miLogin").style.display = "block";
-  document.getElementById("idUsuario").value = "";
-  document.getElementById("idContrasena").value = "";
-
-} else if (x == "no") {
-  document.getElementById("miLogin").style.display = "none";
-}
+    document.getElementById("miLogin").style.display = "block";
+    document.getElementById("idUsuario").value = "";
+    document.getElementById("idContrasena").value = "";
+    document.getElementById("elementos").style.display = "none";
+    document.getElementById("crearNoticia").style.display = "none";
+    document.getElementById("miPerfil").style.display = "none"
+  } else if (x == "no") {
+    document.getElementById("miLogin").style.display = "none";
+  }
 }
 
 function soloDigitos(e) {
-    var keychar;
-    var teclanum;
+  var keychar;
+  var teclanum;
 
-    teclanum = e.which;
-    keychar = String.fromCharCode(teclanum);
+  teclanum = e.which;
+  keychar = String.fromCharCode(teclanum);
 
-    if (keychar < "0" || keychar > "9") {
-        return false;
-    } else {
-        return true;
-    }
+  if (keychar < "0" || keychar > "9") {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function comprobarUsuario() {
@@ -33,27 +35,28 @@ function comprobarUsuario() {
   var existe = false;
 
   if (!(usuario == "") && !(pass == "")) {
-  for (i = 0; i < misUsuarios.length; i++) {
-    if (misUsuarios[i] == usuario) {
-      if (misContrasenas[i] == pass) {
-        existe = true;
-        document.getElementById("miPerfil").innerHTML = usuario;
-        mostrarLogin("no");
-        mostrarNoticias();
-        if (misRoles[i] == "admin") {
-          mostrarInsertarNoticia();
-        } else {
-          document.getElementById("crearNoticia").style.display = "none";
+    for (i = 0; i < misUsuarios.length; i++) {
+      if (misUsuarios[i] == usuario) {
+        if (misContrasenas[i] == pass) {
+          existe = true;
+          document.getElementById("miPerfil").innerHTML = usuario;
+          document.getElementById("miPerfil").style.display = "block"
+          mostrarLogin("no");
+          mostrarNoticias();
+          if (misRoles[i] == "admin") {
+            mostrarInsertarNoticia();
+          } else {
+            document.getElementById("crearNoticia").style.display = "none";
+          }
         }
       }
     }
+    if (!existe) {
+      alert("El usuario o la contraseña no existen");
+    }
+  } else {
+    alert("Los campos no pueden estar vacios");
   }
-  if (!existe) {
-    alert("El usuario o la contraseña no existen");
-  }
-} else {
-  alert("Los campos no pueden estar vacios");
-}
 }
 
 function mostrarNoticias() {
@@ -71,6 +74,5 @@ function aniadirNoticia() {
   var div = document.createElement("div");
   div.className = "texto";
   div.innerHTML = "<h3>" + titulo + "</h3><p>" + noticia + "</p>";
-
-document.getElementById("elementos").appendChild(div);
+  document.getElementById("elementos").appendChild(div);
 }
